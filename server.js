700be9956d8fd233c4a1d01748ce451b72e9b52f
Jacob Bohlmann <jacobbohlmann45.com>
2026-03-10 12:29:31 -0400

    Broken JSON attempt 1

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
-
+const data = require("./data-service");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const multer = require("multer");
 
 const app = express();
 dotenv.config();
@@ -43,11 +46,40 @@ app.get("/accountcreation.html", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "accountcreation.html"));
 });
 
+app.get("/employeepsa", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "employeepsa.html"));
+});
+
+app.get("/createshipment", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "createshipment.html"));
+});
+
+app.post("/createshipment", (req, res) => {
+  data.addShipment(req.body).then(() => {
+    res.redirect("/createshipment");
+  });
+});
+
+app.get("/createorder", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "createorder.html"));
+});
+
+app.post("/createorder", (req, res) => {
+  data.addOrder(req.body).then(() => {
+    res.redirect("/createorder");
+  });
+});
 
 // run "node server.js" to start the setup server
 
-// setup server
-app.listen(HTTP_PORT, function () {
-  console.log(`App listening on port: ${HTTP_PORT}`);
-});
-  
+// setup server 
+data
+  .initialize()
+  .then(function () {
+    app.listen(HTTP_PORT, function () {
+      console.log(`App listening on port: ${HTTP_PORT}`);
+    });
+  })
+  .catch(function (err) {
+    console.log(`Unable to start server: ${err}`);
+  });
